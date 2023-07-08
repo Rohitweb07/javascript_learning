bc9a9050db0e1509b13e96c0a8678a90beb3ae22
Rohit <rohitkushwaha137@gmail.com>
2023-07-08 14:43:11 +0530

    adding some more method of array

--- jsfile/52.js
+++ jsfile/52.js
@@ -5,7 +5,7 @@ const arr = [3, 4, 5, 6, 8, 1, 30, 11, 34];
 const result1 = arr.reduce((accumuator, currentvale) => {
   return accumuator + currentvale;
 });
-console.log(result1);
++console.log(result1);
 
 const result2 = arr.reduce((accumuator, currentvale) => {
   return accumuator + currentvale;
@@ -16,6 +16,7 @@ const addtoCart = [
   { product_id: 101, product_name: "shoes", price: 12000 },
   { product_id: 201, product_name: "shirt", price: 22000 },
   { product_id: 301, product_name: "T-shirt", price: 32000 },
+  { product_id: 401, product_name: "cap", price: 34000 },
 ];
 
 const res = addtoCart.reduce((totalprice, currentproductPrice) => {
--- jsfile/53.js
+++ jsfile/53.js
@@ -1,1 +1,14 @@
 // sort method for array
+const arr = [50, 1000, 300, 20, 500];
+
+const res = arr.sort();
+console.log(res);
+
+const personName = ["rohit", "mohit", "kumar", "abc"];
+const res2 = personName.sort();
+console.log(res2);
+
+const expectres = arr.sort((a, b) => {
+  return a - b;
+});
+console.log(expectres);
